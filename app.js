8a32c0810e10fc0be82a3ecbfe30ea15ddf6d0fe
phikwi <philipkwizera@gmail.com>
2020-03-02 15:16:58 +0100

    WW

--- app.js
+++ app.js
@@ -167,18 +167,7 @@ async function scrape(p){
         const browser = await puppeteer.launch({ headless: true});
         const page = await browser.newPage();
         await page.setViewport({ width: 1920, height: 1080 });
-        await page.setRequestInterception(true);
-         
-        page.on('request', (req) => {
-          if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image'){
-              req.abort();
-          }
-          else {
-              req.continue();
-          }
-        });
-    
-    
+      
          await page.goto(`https://www.mtrnordic.se/hittegods/sok-hittegods/?sl=1&sl=3&pageNumber=${p}`, {waitUntil: 'networkidle0'});
        
          let mainNode =  await page.$eval( '#sort-found-and-lost-table',  function(el){
